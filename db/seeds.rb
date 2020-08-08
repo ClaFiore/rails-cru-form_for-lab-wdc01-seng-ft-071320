547bfef6adcfbbc84f9e2aca606807d2337aaed9
ClaFiore <claborghini@gmail.com>
2020-08-08 09:58:44 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+Artist.destroy_all
+Genre.destroy_all
+Song.destroy_all
+
+
+artists = []
+5.times do 
+    artists << Artist.create(name: Faker::Artist.name, bio: Faker::ChuckNorris.fact)
+end
+
+genres = []
+5.times do 
+    genres << Genre.create(name: Faker::Book.genre)
+end
+
+
+10.times do 
+    Song.create(name: Faker::Superhero.name,
+    artist: artists.sample,
+    genre: genres.sample
+    )
+end
+
+
+    
